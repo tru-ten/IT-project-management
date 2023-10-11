22c0e189d6159ec5a6acdca159a509e167a80043
Bohdan Zhemelko <bogdan.zhemelko@gmail.com>
2023-10-11 23:47:23 +0300

    modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 while True:
     name = input('Write your name here: ')
-    if name.isalpha() and len(name) > 2:
-        print(f'Your name is {name.capitalize()}')
+    age = input('Write your age here: ')
+    if (name.isalpha() and len(name) > 2) and (age.isdigit() and 5 < int(age) < 100):
+        print(f'Your name is {name.capitalize()} and you are {age} years old')
         break
-    else:
-        print('Failed to initialize name. Try again.\n')
+    elif not (name.isalpha() and len(name) > 2):
+        print('Failed to initialize name\n')
+    elif not (age.isdigit() and 5 < int(age) < 100):
+        print('Failed to initialize age\n')
